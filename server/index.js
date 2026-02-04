275269cd568db37a0987f1febc7538d2ac176fea
Alen-Eftimov <alen.eftimov@gmail.com>
2026-02-04 01:05:15 +0100

    update server

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ app.use(cors())
 //Middleware to parse incoming JSON requests
 app.use(express.json())
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from the server!' });
+app.get('/', (req, res) => {
+    res.json({ message: 'API is working!' });
 });
 
 app.post("/api/send", (req, res) => {
